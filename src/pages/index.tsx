1473fc422b1d1e728bb0333bacbf04ee2100eadf
Tilap <tilap@users.noreply.github.com>
2021-12-05 12:06:30 +0100

    chore: use absolute import within src instead of relative paths (#3)
    Absolute import with:
    
    - typescript with ` baseUr`
    - jest with `moduleDirectories`
    - replace existing relative import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { useApolloClient } from '@apollo/client';
 import type { NextPage } from 'next';
 import { useTranslation } from 'react-i18next';
 
-import { i18n, Language } from '../lib/i18n';
+import { i18n, Language } from 'lib/i18n';
 
 const handleClick: React.MouseEventHandler = () => {
   const currentLanguage = i18n.language;
